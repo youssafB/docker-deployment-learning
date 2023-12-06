5c0be72f1816c69b0f3d9fded47d8f2f76ffbe76
youss@f <119492948+youssafB@users.noreply.github.com>
2023-12-06 17:44:57 +0100

    Update app.py

--- app.py
+++ app.py
@@ -3,13 +3,13 @@ from flask import Flask, render_template, request
 app = Flask(__name__)
 
 @app.route('/')
-def index():
+def home():
     return render_template('index.html')
 
 @app.route('/greet', methods=['POST'])
 def greet():
-    name = request.form.get('name', 'Guest')
-    return render_template('greet.html', name=name)
+    name = request.form.get('name')
+    return f'Hello, {name}!'
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True, host='0.0.0.0')
